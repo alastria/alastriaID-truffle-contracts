723f165274ea2b5354cb2a05b7db821327002792
miguelGomezLZPF <miguel.gomez@izertis.com>
2020-12-15 10:33:56 +0100

    added return ABI encoded data as bytes to delegate calls

--- contracts/identityManager/AlastriaProxy.sol
+++ contracts/identityManager/AlastriaProxy.sol
@@ -5,7 +5,7 @@ import "../libs/Owned.sol";
 contract AlastriaProxy is Owned {
     //address public owner;
 
-    event Forwarded (address indexed destination, uint value, bytes data);
+    event Forwarded(address indexed destination, uint256 value, bytes data, bytes result);
 
     //TODO: upgradeable owner for version in Identity Manager
     //constructor () public {
@@ -16,10 +16,14 @@ contract AlastriaProxy is Owned {
         revert();
     }
 
-    function forward(address destination, uint value, bytes memory data) public onlyOwner {
-        bool ret; 
-        (ret, ) = destination.call.value(value)(data);
+    function forward(
+        address destination,
+        uint256 value,
+        bytes memory data
+    ) public onlyOwner returns(bytes memory) {
+        (bool ret, bytes memory result) = destination.call.value(value)(data);
         require(ret);
-        emit Forwarded(destination, value, data);
+        emit Forwarded(destination, value, data, result);
+        return result;
     }
 }
