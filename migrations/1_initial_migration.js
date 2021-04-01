6ea42b0036af1e806902e96f8665b49dd47a5c00
Roberto <robertgar1993@hotmail.com>
2021-04-01 12:27:07 +0200

    the airbnb standard is added to the scripts in the migrations folder.

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("contracts/misc/Migrations.sol");
+const Migrations = artifacts.require('contracts/misc/Migrations.sol');
 
-module.exports = function(deployer) {
+module.exports = function (deployer) {
   deployer.deploy(Migrations);
 };
--- migrations/4_initialize_contracts.js
+++ migrations/4_initialize_contracts.js
@@ -1,34 +1,67 @@
-const fs = require('fs')
+const fs = require('fs');
 const TruffleConfig = require('@truffle/config');
-const AlastriaIdentityManager = artifacts.require('contracts/identityManager/AlastriaIdentityManager.sol');
-const AlastriaCredentialRegistry = artifacts.require('contracts/registry/AlastriaCredentialRegistry.sol');
-const AlastriaPublicKeyRegistry = artifacts.require('contracts/registry/AlastriaPublicKeyRegistry.sol');
-const AlastriaPresentationRegistry  = artifacts.require('contracts/registry/AlastriaPresentationRegistry.sol');
 
-module.exports = async function(callback) {
+const AlastriaIdentityManager = artifacts.require(
+  'contracts/identityManager/AlastriaIdentityManager.sol'
+);
+const AlastriaCredentialRegistry = artifacts.require(
+  'contracts/registry/AlastriaCredentialRegistry.sol'
+);
+const AlastriaPublicKeyRegistry = artifacts.require(
+  'contracts/registry/AlastriaPublicKeyRegistry.sol'
+);
+const AlastriaPresentationRegistry = artifacts.require(
+  'contracts/registry/AlastriaPresentationRegistry.sol'
+);
+
+module.exports = async function (callback) {
   const addresses = JSON.parse(fs.readFileSync('./addresses.json'));
   const config = TruffleConfig.detect().env;
 
   try {
-    credentialRegistry = await AlastriaCredentialRegistry.at(addresses[config.credential])
-    let tx = await credentialRegistry.initialize('0x0000000000000000000000000000000000000001')
-    console.log(`Credential registry initilized in ${tx.receipt.transactionHash}`)
+    credentialRegistry = await AlastriaCredentialRegistry.at(
+      addresses[config.credential]
+    );
+    let tx = await credentialRegistry.initialize(
+      '0x0000000000000000000000000000000000000001'
+    );
+    console.log(
+      `Credential registry initilized in ${tx.receipt.transactionHash}`
+    );
 
-    presentationRegistry = await AlastriaPresentationRegistry.at(addresses[config.presentation])
-    tx = await presentationRegistry.initialize('0x0000000000000000000000000000000000000001')
-    console.log(`Presentation registry initilized in ${tx.receipt.transactionHash}`)
+    presentationRegistry = await AlastriaPresentationRegistry.at(
+      addresses[config.presentation]
+    );
+    tx = await presentationRegistry.initialize(
+      '0x0000000000000000000000000000000000000001'
+    );
+    console.log(
+      `Presentation registry initilized in ${tx.receipt.transactionHash}`
+    );
 
-    publicKeyRegistry = await AlastriaPublicKeyRegistry.at(addresses[config.publicKey])
-    tx = await publicKeyRegistry.initialize('0x0000000000000000000000000000000000000001')
-    console.log(`Public key registry initilized in ${tx.receipt.transactionHash}`)
+    publicKeyRegistry = await AlastriaPublicKeyRegistry.at(
+      addresses[config.publicKey]
+    );
+    tx = await publicKeyRegistry.initialize(
+      '0x0000000000000000000000000000000000000001'
+    );
+    console.log(
+      `Public key registry initilized in ${tx.receipt.transactionHash}`
+    );
 
-    identityManager = await AlastriaIdentityManager.at(addresses[config.manager])
-    tx = await identityManager.initialize(addresses[config.credential], addresses[config.publicKey], addresses[config.presentation], config.firstIdentityWallet)
-    console.log(`Identity manager initilized in ${tx.receipt.transactionHash}`)
-
-  } catch(err) {
-    console.log("ERROR:", err)
-    callback(err,null);
+    identityManager = await AlastriaIdentityManager.at(
+      addresses[config.manager]
+    );
+    tx = await identityManager.initialize(
+      addresses[config.credential],
+      addresses[config.publicKey],
+      addresses[config.presentation],
+      config.firstIdentityWallet
+    );
+    console.log(`Identity manager initilized in ${tx.receipt.transactionHash}`);
+  } catch (err) {
+    console.log('ERROR:', err);
+    callback(err, null);
   }
 
   console.log('Contracts initialized');
