f7c83a3703fea0224b1f9062bf02c419e206acd4
cmoralesdiego <63613919+cmoralesdiego@users.noreply.github.com>
2021-07-07 11:47:37 +0200

    Update AlastriaIdentityManager.sol

--- contracts/identityManager/AlastriaIdentityManager.sol
+++ contracts/identityManager/AlastriaIdentityManager.sol
@@ -60,21 +60,31 @@ contract AlastriaIdentityManager is AlastriaIdentityServiceProvider, AlastriaIde
     }
 
     //Methods
+    /*
+    * @dev Generate the Access Token for Alastria Identity
+    * @param _signAddress the address which will have the access token
+    */
     function prepareAlastriaID(address _signAddress) public onlyIdentityIssuer(msg.sender) {
         pendingIDs[_signAddress] = block.timestamp + timeToLive;
         emit PreparedAlastriaID(_signAddress);
     }
-
-    /// @dev Creates a new AlastriaProxy contract for an owner and recovery and allows an initial forward call which would be to set the registry in our case
-    /// @param addPublicKeyCallData of the call to addKey function in AlastriaPublicKeyRegistry from the new deployed AlastriaProxy contract
+    
+    /*
+    * @dev Creates a new AlastriaProxy contract for an owner and recovery and allows an initial forward call which would be to set the registry in our case
+    * @param addPublicKeyCallData of the call to addKey function in AlastriaPublicKeyRegistry from the new deployed AlastriaProxy contract
+    */
     function createAlastriaIdentity(bytes memory addPublicKeyCallData) public validAddress(msg.sender) isPendingAndOnTime(msg.sender) {
         AlastriaProxy identity = new AlastriaProxy();
         identityKeys[msg.sender] = address(identity);
         pendingIDs[msg.sender] = 0;
         identity.forward(address(alastriaPublicKeyRegistry), 0, addPublicKeyCallData);//must be alastria registry call
     }
-
-    /// @dev This method send a transaction trough the proxy of the sender
+   /*
+    * @dev This method send a transaction trough the proxy of the sender
+    * @param _destination
+    * @param _value
+    * @param _data
+    */
     function delegateCall(
         address _destination,
         uint256 _value,
